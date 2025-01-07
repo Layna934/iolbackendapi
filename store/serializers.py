0b8e0506fcb61620a810d0d2f3881bfd677c105e
Cilla Azil <azilafupriscilla@gmail.com>
2025-01-07 22:51:02 +0000

    updated serializers

--- store/serializers.py
+++ store/serializers.py
@@ -24,3 +24,18 @@ class OrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Order
         fields = '__all__'
+
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'password', 'email']
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            password=validated_data['password'],
+            email=validated_data.get('email', '')
+        )
+        return user
