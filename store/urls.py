38bced783857b30e748c2b54e5b0030edd508d85
Cilla Azil <azilafupriscilla@gmail.com>
2025-01-07 20:33:07 +0000

    updated auth for urls.py

--- store/urls.py
+++ store/urls.py
@@ -10,4 +10,7 @@ router.register(r'orders', OrderViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('pay/', FlutterwavePaymentView.as_view(), name='flutterwave_payment'),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', TokenObtainPairView.as_view(), name='login'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
